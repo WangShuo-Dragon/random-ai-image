ff6c4211b1a851f434f873591c15a94b225bc7c3
Wang Shuo <wangshuoliang@hotmail.com>
2022-12-24 19:55:39 +0800

    sample code

---
+++ controllers/openaiController.js
@@ -0,0 +1,31 @@
+const { Configuration, OpenAIApi } = require( "openai" );
+
+const configuration = new Configuration( {
+    apiKey: process.env.OPENAI_API_KEY
+} );
+const openai = new OpenAIApi( configuration );
+
+const generateImage = async ( req, res ) => {
+    try {
+        const response = await openai.createImage({
+            prompt: 'Christmas tree Marina Bay Sands',
+            n: 1,
+            size: '512x512'
+        })
+        const imageUrl = response.data.data[0].url;
+
+        res.status(200).json({
+            success: true,
+            data: imageUrl
+        });
+    } catch ( error ) {
+        res.status(400).json({
+            success: false,
+            error: 'Image cannot be generated'
+        });
+    }
+};
+
+module.exports = {
+    generateImage
+};
